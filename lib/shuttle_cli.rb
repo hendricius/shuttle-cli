b670cbe47d1f28ee5f8dfcc3980d6d421c0aefd9
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2017-02-07 21:32:29 +0100

    bump version and fix requiring pry

--- lib/shuttle_cli.rb
+++ lib/shuttle_cli.rb
@@ -1,13 +1,11 @@
-module ShuttleCli
-  # Your code goes here...
-end
-
 require 'terminal-table'
 require 'json'
-require 'pry'
 require 'active_support/core_ext/hash/indifferent_access'
 
 require_relative 'shuttle_cli/version'
 require_relative 'shuttle_cli/printer'
 require_relative 'shuttle_cli/reader'
 require_relative 'shuttle_cli/bookmark'
+
+module ShuttleCli
+end
--- lib/shuttle_cli/version.rb
+++ lib/shuttle_cli/version.rb
@@ -1,3 +1,3 @@
 module ShuttleCli
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
